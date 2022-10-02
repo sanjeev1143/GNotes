63ad062621ad94abe8eb2a2e637d5b6e6d51a25c
B Sanjeev Reddy <sanjeevteja143@gmail.com>
2022-10-02 17:47:28 +0530

    Adding search bar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,10 +3,49 @@ import DescriptionIcon from '@mui/icons-material/Description';
 import DehazeIcon from '@mui/icons-material/Dehaze';
 import Button from '@mui/material/Button';
 import SideBar from './SideBar';
+import { Db } from './firebase_cofig';
+import SearchIcon from '@mui/icons-material/Search';
+import { collection, getDocs } from "firebase/firestore";
+
+function Card(props) {
+    return (
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <h3>{props.title}</h3> <p>{props.content}</p>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    );
+}
+
 function Navbar() {
+    const [read, setRead] = useState([]);
+    const [load, setLoad] = useState(false);
+    const notesRef = collection(Db, "Notes");
+    useEffect(() => {
+        const getNotes = async () => {
+            try {
+                setLoad(false);
+                const data = await getDocs(notesRef);
+                setRead(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+            } catch (e) {
+                setLoad(true);
+            }
+        };
+        getNotes();
+    }, [read], [load])
+    const [search, setSearch] = useState("");
+    const [found, setFound] = useState([]);
+    function onSearch(e) {
+        setSearch(e.target.value)
+    }
+    function searchData() {
+
+        setFound(read.filter((value) => value.title.toLowerCase() == search.toLocaleLowerCase()));
+        setSearch("");
+
+    }
+
     return (
         <div className='navBar'>
-
 
             <nav class="navbar navbar-expand-lg ">
                 <div class="container-fluid column justify-content-start">
@@ -16,12 +55,12 @@ function Navbar() {
                         </Button>
                         <SideBar />
                     </div>
-
                     <DescriptionIcon htmlColor='#FFF' fontSize='large' className='discIcon' />
                     <h1>Notes</h1>
-
+                    <input onChange={onSearch} placeholder="Enter the title you want to search..." className='searchIp' value={search} /><Button onClick={searchData}><SearchIcon fontSize='large' htmlColor='white' /></Button>
                 </div>
             </nav>
+            {found.length === 0 ? <Card title="Invalid Search" content="" /> : <div>{found.map((value) => <Card title={value.title} content={value.content} />)}</div>}
         </div>
     )
 }
